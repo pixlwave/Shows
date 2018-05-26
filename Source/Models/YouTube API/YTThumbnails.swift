8ed2c806944ba735ccfa4ac1080990b19450f83e
Doug <git@pixlwave.uk>
2018-05-26 20:43:34 +0100

    Update code layout for consistency.

--- Source/Models/YouTube API/YTThumbnails.swift
+++ Source/Models/YouTube API/YTThumbnails.swift
@@ -20,13 +20,13 @@ struct YTThumbnails: Codable {
         let width: UInt
         let height: UInt
         
-        // FIXME: `= nil` added for Swift 4.0 bug
-        var image: UIImage? = nil
-        
         enum CodingKeys: String, CodingKey {
             case url
             case width
             case height
         }
+        
+        // FIXME: `= nil` added for Swift 4.0 bug
+        var image: UIImage? = nil
     }
 }
