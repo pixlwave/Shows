a38da2b266ffd7f30f9fa0a7c21f04aa5c611f75
Doug <git@pixlwave.uk>
2018-06-03 17:11:47 +0100

    Replaces iCloud KVS with CloudKit.

--- Source/Models/UserData.swift
+++ Source/Models/UserData.swift
@@ -1,27 +1,42 @@
-import Foundation
+import CloudKit
 
 class UserData {
     
-    private static var store = NSUbiquitousKeyValueStore.default
+    private static var userDB = CKContainer.default().privateCloudDatabase
     
-    static var watchedList: [String: Bool] = store.dictionary(forKey: "watched") as? [String: Bool] ?? [String: Bool]() {
-        didSet {
-            store.set(watchedList, forKey: "watched")
-        }
-    }
+    static var watchedList: [String] = [String]()
     
     static func reloadWatchedList() {
-        if let newList = store.dictionary(forKey: "watched") as? [String: Bool] {
+        let query = CKQuery(recordType: "YouTubeVideo", predicate: NSPredicate(value: true))
+        userDB.perform(query, inZoneWith: nil) { (records, error) in
+            guard let records = records else { print("Error: \(error)"); return }
+            let newList = records.map { $0.recordID.recordName }
             watchedList = newList
         }
     }
     
     static func queryWatchedStatus(of videoID: String) -> Bool {
-        return watchedList[videoID] ?? false
+        return watchedList.contains(videoID)
     }
     
     static func mark(_ videoID: String, as watched: Bool) {
-        watchedList[videoID] = watched
+        let videoRecordID = CKRecordID(recordName: videoID)
+        if watched == true {
+            watchedList.append(videoID)
+            
+            let videoRecord = CKRecord(recordType: "YouTubeVideo", recordID: videoRecordID)
+            userDB.save(videoRecord) { video, error in
+                if let error = error { print("Error \(error)"); return }
+                print("Success: \(videoID) Saved")
+            }
+        } else {
+            if let index = watchedList.index(of: videoID) { watchedList.remove(at: index) }
+            
+            userDB.delete(withRecordID: videoRecordID) { record, error in
+                if let error = error { print("Error \(error)"); return }
+                print("Success: \(videoID) Deleted")
+            }
+        }
     }
     
 }
