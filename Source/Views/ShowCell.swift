74bb8620035efe7e7f0f1ee90d8a0c9ffc97afff
Doug <git@pixlwave.uk>
2019-06-12 17:12:58 +0100

    Remove image from cells before reuse.

--- Source/Views/ShowCell.swift
+++ Source/Views/ShowCell.swift
@@ -3,4 +3,9 @@ import UIKit
 class ShowCell: UICollectionViewCell {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
 }
--- Source/Views/VideoCell.swift
+++ Source/Views/VideoCell.swift
@@ -5,4 +5,9 @@ class VideoCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var publishedAtLabel: UILabel!
     @IBOutlet weak var watchedLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
 }
